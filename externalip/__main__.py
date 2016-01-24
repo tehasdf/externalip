60bcd4e335622fb811355f6801e8ad93fea70943
asdf <tehasdf@gmail.com>
2016-01-24 20:10:29 +0100

    dont connect to localhost

--- externalip/__main__.py
+++ externalip/__main__.py
@@ -3,4 +3,4 @@ from __future__ import print_function
 from externalip.blocking_client import get_external_ip
 
 
-print(get_external_ip('127.0.0.1:10050'))
+print(get_external_ip(('46.101.132.244', 10050)))
